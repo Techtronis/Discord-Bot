79528965812f8bc32e5a5a08eb637b1012152b33
Alexis Jost <alexisjost20@gmail.com>
2023-06-18 19:19:25 +0200

    i added a bot made by chatgpt (it sucks)

--- Discord-GPT3/bot.py
+++ Discord-GPT3/bot.py
@@ -1,20 +1,22 @@
 import discord
 import openai
+from discord.ext import commands
 
-openai.api_key = "Put openAI key here"
+openai.api_key = ""
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-token = "Put discord key here"
+token = ""
 
-bot = discord.Client(intents = intents)
+gpt = discord.Client(intents = intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.event
+@gpt.event
 async def on_ready():
     print("Bot is connected.")
 
-@bot.event
+@gpt.event
 async def on_message(message):
 
      username = str(message.author).split('#')[0]
@@ -23,18 +25,21 @@ async def on_message(message):
 
      print(username + ": " + user_message)
 
-     if message.author == bot.user:
+     if message.author == gpt.user:
+          return
+     
+     if channel != "chat-gpt":
           return
      
   
      response = openai.Completion.create(
           model = "text-davinci-003",
           prompt = user_message,
-          max_tokens = 3000,
+          max_tokens = 1000,
           temperature =0.7
           )
 
      output = response["choices"][0]["text"]
      await message.channel.send(output)
    
-bot.run(token)
+gpt.run(token)
